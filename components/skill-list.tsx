cd9d200fe479199800342f91597dbeb3ff4ad8c9
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2020-10-03 21:01:58 +0100

    feat: add skill list component

---
+++ components/skill-list.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import CSS3Logo from '@/assets/svg/css3-logo.svg'
+import GitLogo from '@/assets/svg/git-logo.svg'
+import HTML5Logo from '@/assets/svg/html5-logo.svg'
+import JavaScriptLogo from '@/assets/svg/javascript-logo.svg'
+import NextJSLogo from '@/assets/svg/nextjs-logo.svg'
+import NodeJSLogo from '@/assets/svg/nodejs-logo.svg'
+import ReactLogo from '@/assets/svg/react-logo.svg'
+import RokuLogo from '@/assets/svg/roku-logo.svg'
+import SassLogo from '@/assets/svg/sass-logo.svg'
+import TypeScriptLogo from '@/assets/svg/typescript-logo.svg'
+
+interface SkillListItemProps {
+    label: string
+    Icon: string
+}
+
+const SkillListItem = ({ label, Icon }: SkillListItemProps) => (
+    <li className="flex flex-col items-center my-4">
+        <Icon />
+        <p className="my-2">{label}</p>
+    </li>
+)
+
+const SkillList = () => (
+    <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5">
+        <SkillListItem label="JavaScript" Icon={JavaScriptLogo} />
+        <SkillListItem label="TypeScript" Icon={TypeScriptLogo} />
+        <SkillListItem label="React.js" Icon={ReactLogo} />
+        <SkillListItem label="Next.js" Icon={NextJSLogo} />
+        <SkillListItem label="Git" Icon={GitLogo} />
+        <SkillListItem label="HTML" Icon={HTML5Logo} />
+        <SkillListItem label="CSS" Icon={CSS3Logo} />
+        <SkillListItem label="Sass" Icon={SassLogo} />
+        <SkillListItem label="Node.js" Icon={NodeJSLogo} />
+        <SkillListItem label="BrightScript" Icon={RokuLogo} />
+    </ul>
+)
+
+export default SkillList
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,11 @@ import React from 'react'
 import Section from '@/components/section'
 
 import SummaryMarkup from '@/components/sections/summary.mdx'
-import ProjectsMarkup from '@/components/sections/projects.mdx'
-import ContactMarkup from '@/components/sections/contact.mdx'
 
 const HomePage = () => (
-    <main className="flex flex-col items-center p-6 sm:px-8 divide-y divide-gray-300">
+    <main className="flex flex-col items-center p-6 sm:px-8 min-h-screen divide-y divide-gray-300">
         <Section id="summary">
             <SummaryMarkup />
-        </Section>
-        <Section id="projects">
-            <ProjectsMarkup />
-        </Section>
-        <Section id="contact">
-            <ContactMarkup />
         </Section>
     </main>
 )
