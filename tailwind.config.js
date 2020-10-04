8c60634694935eaa9c4d172c5e9a93fda42338b4
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2020-10-04 17:44:50 +0100

    fix: update tailwind purge directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
         purgeLayersByDefault: true,
         removeDeprecatedGapUtilities: true
     },
-    purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+    purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
     theme: {
         darkSelector: '.dark-mode',
         extend: {
