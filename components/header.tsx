1f2b1367984f91d2ca44c6675a3923783b8a1589
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2020-11-20 18:15:35 +0000

    refactor: add explicit dimensions to header’s <img/>

--- components/header.tsx
+++ components/header.tsx
@@ -15,6 +15,8 @@ const Header = () => {
                     alt="Julio Alves"
                     className="w-12 h-auto border-2 border-neon-blue dark:border-cerise rounded-full"
                     src={profileImg}
+                    width="48"
+                    height="48"
                 />
                 <span className="pl-3 text-neon-blue dark:text-cerise text-xl font-semibold">juliomalves.dev</span>
             </Link>
