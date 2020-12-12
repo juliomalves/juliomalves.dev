0fc0fd7c049885f11d4afabb4898e7ba5c377e5a
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2020-12-12 17:48:48 +0000

    refactor: simplify theme-toggle-button conditions

--- components/theme-toggle-button.tsx
+++ components/theme-toggle-button.tsx
@@ -5,11 +5,11 @@ import SvgIcon, { moonIcon, sunIcon } from '@/components/svg-icon'
 const ThemeToggleButton = () => {
     const [isDarkMode, setDarkMode] = useDarkMode({ storageKey: 'jma-theme' })
     const toggleMode = () => setDarkMode(!isDarkMode)
-    const label = isDarkMode ? 'Light mode' : 'Dark mode'
+    const [label, icon] = isDarkMode ? ['Light mode', sunIcon] : ['Dark mode', moonIcon]
 
     return (
         <button className="flex p-2 text-yellow-400 focus-outline hover:glow" onClick={toggleMode} aria-label={label}>
-            <SvgIcon className="w-5" d={isDarkMode ? sunIcon : moonIcon} viewBox="0 0 512 512" />
+            <SvgIcon className="w-5" d={icon} viewBox="0 0 512 512" />
         </button>
     )
 }
