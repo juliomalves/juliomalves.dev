015a831ab076363362eca429f9a78908f522a2b7
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2021-01-23 16:49:53 +0000

    refactor: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const withPlugins = require('next-compose-plugins')
 const withPrefresh = require('@prefresh/next')
 const withImages = require('next-images')
@@ -6,55 +5,43 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE_BUNDLE === 'true'
 })
 
-module.exports = withPlugins(
-    [
-        [
-            withImages,
-            {
-                exclude: path.resolve(__dirname, 'assets/svg')
+module.exports = withPlugins([withImages, withPrefresh, withBundleAnalyzer], {
+    poweredByHeader: false,
+    webpack: (config, { dev, isServer }) => {
+        // Move Preact into the framework chunk instead of duplicating in routes
+        const splitChunks = config.optimization && config.optimization.splitChunks
+        if (splitChunks) {
+            const cacheGroups = splitChunks.cacheGroups
+            const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
+            if (cacheGroups.framework) {
+                cacheGroups.preact = { ...cacheGroups.framework, test }
+                // Merge the 2 small commons+framework chunks
+                cacheGroups.commons.name = 'framework'
             }
-        ],
-        withPrefresh,
-        withBundleAnalyzer
-    ],
-    {
-        poweredByHeader: false,
-        webpack: (config, { dev, isServer }) => {
-            // Move Preact into the framework chunk instead of duplicating in routes
-            const splitChunks = config.optimization && config.optimization.splitChunks
-            if (splitChunks) {
-                const cacheGroups = splitChunks.cacheGroups
-                const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-                if (cacheGroups.framework) {
-                    cacheGroups.preact = { ...cacheGroups.framework, test }
-                    // Merge the 2 small commons+framework chunks
-                    cacheGroups.commons.name = 'framework'
-                }
-            }
-
-            // Mark Preact libs as external for server bundle to prevent duplicate copies of preact
-            if (isServer) {
-                config.externals.push(/^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/)
-            }
-
-            // Setup webpack aliases
-            config.resolve.alias = {
-                ...(config.resolve.alias || {}),
-                react: 'preact/compat',
-                'react-dom': 'preact/compat'
-            }
-
-            // Automatically inject Preact devtools
-            if (dev && !isServer) {
-                const entry = config.entry
-                config.entry = () =>
-                    entry().then(entries => {
-                        entries['main.js'] = ['preact/debug', ...(entries['main.js'] || [])]
-                        return entries
-                    })
-            }
-
-            return config
         }
+
+        // Mark Preact libs as external for server bundle to prevent duplicate copies of preact
+        if (isServer) {
+            config.externals.push(/^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/)
+        }
+
+        // Setup webpack aliases
+        config.resolve.alias = {
+            ...(config.resolve.alias || {}),
+            react: 'preact/compat',
+            'react-dom': 'preact/compat'
+        }
+
+        // Automatically inject Preact devtools
+        if (dev && !isServer) {
+            const entry = config.entry
+            config.entry = () =>
+                entry().then(entries => {
+                    entries['main.js'] = ['preact/debug', ...(entries['main.js'] || [])]
+                    return entries
+                })
+        }
+
+        return config
     }
-)
+})
