5bd1efcb88b7ed0713e01ff39476950273afd0be
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2021-05-03 14:35:52 +0100

    chore: remove theme script from metadata

--- components/metadata.tsx
+++ components/metadata.tsx
@@ -32,29 +32,6 @@ const Metadata = () => (
         <meta name="twitter:site" content={twitterHandle} />
         <meta name="twitter:creator" content={twitterHandle} />
         <meta name="twitter:card" content="summary_large_image" />
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                    (function() {
-                        const storageKey = 'jma-theme';
-                        const darkThemeClass = 'dark';
-                        const lightThemeClass = 'light';
-                        const prefersDarkColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-                        const storedTheme = localStorage.getItem(storageKey);
-                        function setTheme(isDarkTheme) {
-                            document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
-                            document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
-                        }
-                        if (storedTheme) {
-                            setTheme(storedTheme === darkThemeClass);
-                        } else {
-                            setTheme(prefersDarkColorScheme);
-                            localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
-                        }
-                    })();
-                `
-            }}
-        />
     </Head>
 )
 
