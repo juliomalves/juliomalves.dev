8bf29724f89108cf4363374f2cbf329ac06bc80f
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2021-05-03 12:05:02 +0100

    test: update header and theme-toggle-button tests

--- tests/specs/components/theme-toggle-button.test.tsx
+++ tests/specs/components/theme-toggle-button.test.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import { fireEvent, render, screen } from '@testing-library/preact'
 import ThemeToggleButton from '@/components/theme-toggle-button'
+import { ThemeProvider } from '@/components/contexts/theme'
 
 describe('GIVEN a <ThemeToggleButton />', () => {
     describe('WHEN the button is clicked', () => {
         it('THEN should toggle the theme', () => {
-            render(<ThemeToggleButton />)
+            render(
+                <ThemeProvider>
+                    <ThemeToggleButton />
+                </ThemeProvider>
+            )
             fireEvent.click(screen.getByLabelText('Dark mode'))
             expect(screen.getByLabelText('Light mode')).toBeInTheDocument()
             expect(screen.getByRole('img', { name: 'Sun icon' })).toBeInTheDocument()
