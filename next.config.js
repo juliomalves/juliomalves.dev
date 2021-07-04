e80a441771abaff69b6bb38d21dc2f5642059432
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2021-07-04 16:31:09 +0100

    chore: upgrade to Next.js 11

--- next.config.js
+++ next.config.js
@@ -1,64 +1,12 @@
-const withPrefresh = require('@prefresh/next')
+const withPreact = require('next-plugin-preact')
 const WebpackBar = require('webpackbar')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE_BUNDLE === 'true'
 })
 
-const withPreact = (nextConfig = {}) => {
-    return withPrefresh(
-        Object.assign({}, nextConfig, {
-            webpack: (config, options) => {
-                const { dev, isServer } = options
-                // Move Preact into the framework chunk instead of duplicating in routes
-                const splitChunks = config.optimization && config.optimization.splitChunks
-                if (splitChunks && splitChunks.cacheGroups) {
-                    const cacheGroups = splitChunks.cacheGroups
-                    const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
-                    if (cacheGroups.framework) {
-                        cacheGroups.preact = { ...cacheGroups.framework, test }
-                        // Merge the 2 small commons+framework chunks
-                        cacheGroups.commons.name = 'framework'
-                    }
-                }
-
-                // Mark Preact libs as external for server bundle to prevent duplicate copies of preact
-                if (isServer) {
-                    config.externals.push(/^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/)
-                }
-
-                // Setup webpack aliases
-                config.resolve.alias = {
-                    ...(config.resolve.alias || {}),
-                    react: 'preact/compat',
-                    'react-dom': 'preact/compat'
-                }
-
-                // Automatically inject Preact devtools
-                if (dev && !isServer) {
-                    const entry = config.entry
-                    config.entry = () =>
-                        entry().then(entries => {
-                            entries['main.js'] = ['preact/debug', ...(entries['main.js'] || [])]
-                            return entries
-                        })
-                }
-
-                if (typeof nextConfig.webpack === 'function') {
-                    config = nextConfig.webpack(config, options)
-                }
-
-                return config
-            }
-        })
-    )
-}
-
 module.exports = withBundleAnalyzer(
     withPreact({
         poweredByHeader: false,
-        future: {
-            webpack5: true
-        },
         webpack: (config, { isServer }) => {
             const name = isServer ? 'server' : 'client'
             config.plugins.push(new WebpackBar({ name }))
