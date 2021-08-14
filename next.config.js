b1745826bfd1254e37ec55f60ec37415dde7e31e
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2021-08-14 08:45:13 +0100

    chore: update Next.js dependencies

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer(
     withPreact({
+        reactStrictMode: true,
         poweredByHeader: false,
         webpack: (config, { isServer }) => {
             const name = isServer ? 'server' : 'client'
