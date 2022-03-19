e8bf473740e03d02dcb70f272fa2a0f5d812d5a3
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2022-03-19 23:48:56 +0000

    refactor: simplify `FooterLisItem` component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import Link from '@/components/link'
 import SvgIcon from '@/components/svg-icon'
+import type { IconId } from '@/components/svg-icon'
 
 interface FooterLinkProps {
     href: string
     label: string
+    iconId: IconId
+    iconTitle: string
 }
 
-const FooterLisItem = ({ children, href, label }: React.PropsWithChildren<FooterLinkProps>) => (
+const FooterLisItem = ({ href, label, iconId, iconTitle }: FooterLinkProps) => (
     <li className="p-4">
         <Link
             href={href}
@@ -15,7 +18,7 @@ const FooterLisItem = ({ children, href, label }: React.PropsWithChildren<Footer
             label={label}
             external
         >
-            {children}
+            <SvgIcon className="w-8 h-8" id={iconId} title={iconTitle} />
         </Link>
     </li>
 )
@@ -26,18 +29,20 @@ const Footer = () => {
     return (
         <footer className="flex flex-col justify-center items-center w-full max-w-3xl mx-auto p-4">
             <ul className="flex items-center">
-                <FooterLisItem href="https://www.twitter.com/juli0malves" label="Twitter">
-                    <SvgIcon className="w-8 h-8" id="icon-twitter" title="Twitter logo" />
-                </FooterLisItem>
-                <FooterLisItem href="https://www.github.com/juliomalves" label="GitHub">
-                    <SvgIcon className="w-8 h-8" id="icon-github" title="GitHub logo" />
-                </FooterLisItem>
-                <FooterLisItem href="https://www.stackoverflow.com/users/1870780/juliomalves" label="Stack Overflow">
-                    <SvgIcon className="w-8 h-8" id="icon-stackoverflow" title="Stack Overflow logo" />
-                </FooterLisItem>
-                <FooterLisItem href="https://www.linkedin.com/in/juliomalves" label="LinkedIn">
-                    <SvgIcon className="w-8 h-8" id="icon-linkedin" title="LinkedIn logo" />
-                </FooterLisItem>
+                <FooterLisItem href="https://www.twitter.com/juli0malves" label="Twitter" iconId="icon-twitter" iconTitle="Twitter logo" />
+                <FooterLisItem href="https://www.github.com/juliomalves" label="GitHub" iconId="icon-github" iconTitle="GitHub logo" />
+                <FooterLisItem
+                    href="https://www.stackoverflow.com/users/1870780/juliomalves"
+                    label="Stack Overflow"
+                    iconId="icon-stackoverflow"
+                    iconTitle="Stack Overflow logo"
+                />
+                <FooterLisItem
+                    href="https://www.linkedin.com/in/juliomalves"
+                    label="LinkedIn"
+                    iconId="icon-linkedin"
+                    iconTitle="LinkedIn logo"
+                />
             </ul>
             <p className="text-center text-sm">Julio Alves © {currentYear}</p>
         </footer>
