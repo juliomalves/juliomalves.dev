f1664b9d08020f35fb11af35341f6e236a20da8d
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2022-05-21 20:32:27 +0100

    refactor: convert `_document` to function component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class Document extends NextDocument {
-    render() {
-        return (
-            <Html lang="en">
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+const Document = () => {
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
 
 export default Document
