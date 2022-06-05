d82ad2e5aa414a9a02729f345d6e2f3d3004eb3f
Julio Alves <juliomalves@gmail.com>
2022-06-05 18:05:02 +0100

    refactor: update theme script (#141)
    * refactor: update theme script
    * chore: update CSP

--- components/contexts/theme.tsx
+++ components/contexts/theme.tsx
@@ -59,28 +59,23 @@ export const ThemeProvider = ({ storageKey = STORAGE_KEY, children }: React.Prop
 }
 
 const ThemeScript = () => (
-    <script
-        key="theme"
-        dangerouslySetInnerHTML={{
-            __html: `
-                (function() {
-                    const storageKey = '${STORAGE_KEY}';
-                    const darkThemeClass = 'dark';
-                    const lightThemeClass = 'light';
-                    const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
-                    const storedTheme = localStorage.getItem(storageKey);
-                    function setTheme(isDarkTheme) {
-                        document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
-                        document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
-                    }
-                    if (storedTheme) {
-                        setTheme(storedTheme === darkThemeClass);
-                    } else {
-                        setTheme(prefersDarkColorScheme);
-                        localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
-                    }
-                })();
-            `
-        }}
-    />
+    <script key="theme">
+        {`(function() {
+            const storageKey = '${STORAGE_KEY}';
+            const darkThemeClass = 'dark';
+            const lightThemeClass = 'light';
+            const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
+            const storedTheme = localStorage.getItem(storageKey);
+            function setTheme(isDarkTheme) {
+                document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
+                document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
+            }
+            if (storedTheme) {
+                setTheme(storedTheme === darkThemeClass);
+            } else {
+                setTheme(prefersDarkColorScheme);
+                localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
+            }
+        })();`}
+    </script>
 )
