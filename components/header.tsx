09fd832b14b51c73d2921d2a44469abbff33b023
Julio Alves <juliomalves@gmail.com>
2022-09-02 23:26:52 +0100

    refactor: replace `img` with `next/future/image` (#195)
    * refactor: replace `img` with `next/future/image`
    * test: update tests config

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Link from '@/components/link'
 import dynamic from 'next/dynamic'
+import Image from 'next/future/image'
 
 const ThemeToggleButton = dynamic(() => import('@/components/theme-toggle-button'), {
     ssr: false
@@ -10,12 +11,13 @@ const Header = () => {
     return (
         <header className="sticky top-0 flex flex-row justify-between items-center w-full max-w-3xl mx-auto px-4 tablet:px-8 py-4 bg-white dark:bg-midnight">
             <Link href="/" className="flex flex-row items-center focus-outline" label="juliomalves.dev">
-                <img
+                <Image
                     alt="Julio Alves"
                     className="w-12 h-auto border-2 border-neon-blue dark:border-green rounded-full"
                     src="/images/profile-pic.jpg"
                     width="48"
                     height="48"
+                    priority={true}
                 />
                 <span className="pl-3 text-neon-blue dark:text-green text-xl font-semibold">juliomalves.dev</span>
             </Link>
