891016ae825c29be8917588913224cf89ba3c183
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2022-11-13 19:45:09 +0000

    refactor: move analytics scripts to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { AnalyticsProvider } from '@/components/contexts/analytics'
+import Script from 'next/script'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 
 import '@/styles/app.css'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -12,11 +14,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <script id="theme" src="/scripts/theme.js"></script>
             </head>
             <body>
-                <AnalyticsProvider>
-                    <Header />
-                    {children}
-                    <Footer />
-                </AnalyticsProvider>
+                <Header />
+                {children}
+                <Footer />
+                {isProduction && (
+                    <>
+                        <Script key="gtag" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`} />
+                        <Script id="gtag-config" key="gtag-config">
+                            {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){ dataLayer.push(arguments); }
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {
+                                    page_path: window.location.pathname,
+                                });
+                            `}
+                        </Script>
+                    </>
+                )}
             </body>
         </html>
     )
