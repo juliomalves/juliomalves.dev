d8a9d10388291d6750d5352661befc8108223b6c
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-03-04 22:52:23 +0000

    refactor: move theme script to `_document`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const STORAGE_KEY = 'jma-theme'
+const MEDIA_QUERY = '(prefers-color-scheme: dark)'
+
 const Document = () => {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <script id="theme" key="theme">
+                    {`(function() {
+const storageKey = '${STORAGE_KEY}';
+const darkThemeClass = 'dark';
+const lightThemeClass = 'light';
+const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
+const storedTheme = localStorage.getItem(storageKey);
+function setTheme(isDarkTheme) {
+    document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
+    document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
+}
+if (storedTheme) {
+    setTheme(storedTheme === darkThemeClass);
+} else {
+    setTheme(prefersDarkColorScheme);
+    localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
+}
+})();`}
+                </script>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
