53b242f047da184b9d75deff540fafdb33563f3a
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-03-04 23:15:30 +0000

    chore: cleanup modified files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-const STORAGE_KEY = 'jma-theme'
-const MEDIA_QUERY = '(prefers-color-scheme: dark)'
 const scriptHtml = `(function() {
-    const storageKey = '${STORAGE_KEY}';
+    const storageKey = 'jma-theme';
     const darkThemeClass = 'dark';
     const lightThemeClass = 'light';
-    const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
+    const prefersDarkColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const storedTheme = localStorage.getItem(storageKey);
     function setTheme(isDarkTheme) {
         document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
