5129c763be3abf836684e1017c6a0ce265ca2996
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-03-04 23:09:53 +0000

    refactor: use `dangerouslySetInnerHTML` for theme script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,30 +3,35 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const STORAGE_KEY = 'jma-theme'
 const MEDIA_QUERY = '(prefers-color-scheme: dark)'
+const scriptHtml = `(function() {
+    const storageKey = '${STORAGE_KEY}';
+    const darkThemeClass = 'dark';
+    const lightThemeClass = 'light';
+    const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
+    const storedTheme = localStorage.getItem(storageKey);
+    function setTheme(isDarkTheme) {
+        document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
+        document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
+    }
+    if (storedTheme) {
+        setTheme(storedTheme === darkThemeClass);
+    } else {
+        setTheme(prefersDarkColorScheme);
+        localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
+    }
+})();`
 
 const Document = () => {
     return (
         <Html lang="en">
             <Head>
-                <script id="theme" key="theme">
-                    {`(function() {
-                        const storageKey = '${STORAGE_KEY}';
-                        const darkThemeClass = 'dark';
-                        const lightThemeClass = 'light';
-                        const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
-                        const storedTheme = localStorage.getItem(storageKey);
-                        function setTheme(isDarkTheme) {
-                            document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
-                            document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
-                        }
-                        if (storedTheme) {
-                            setTheme(storedTheme === darkThemeClass);
-                        } else {
-                            setTheme(prefersDarkColorScheme);
-                            localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
-                        }
-                    })();`}
-                </script>
+                <script
+                    id="theme"
+                    key="theme"
+                    dangerouslySetInnerHTML={{
+                        __html: scriptHtml
+                    }}
+                ></script>
             </Head>
             <body>
                 <Main />
