76bfa57cdee3d96be0c81dd9218dac19c3f02f3f
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-03-04 22:57:10 +0000

    chore: format script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,22 +10,22 @@ const Document = () => {
             <Head>
                 <script id="theme" key="theme">
                     {`(function() {
-const storageKey = '${STORAGE_KEY}';
-const darkThemeClass = 'dark';
-const lightThemeClass = 'light';
-const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
-const storedTheme = localStorage.getItem(storageKey);
-function setTheme(isDarkTheme) {
-    document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
-    document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
-}
-if (storedTheme) {
-    setTheme(storedTheme === darkThemeClass);
-} else {
-    setTheme(prefersDarkColorScheme);
-    localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
-}
-})();`}
+                        const storageKey = '${STORAGE_KEY}';
+                        const darkThemeClass = 'dark';
+                        const lightThemeClass = 'light';
+                        const prefersDarkColorScheme = window.matchMedia('${MEDIA_QUERY}').matches;
+                        const storedTheme = localStorage.getItem(storageKey);
+                        function setTheme(isDarkTheme) {
+                            document.documentElement.classList.add(isDarkTheme ? darkThemeClass : lightThemeClass);
+                            document.documentElement.classList.remove(isDarkTheme ? lightThemeClass : darkThemeClass);
+                        }
+                        if (storedTheme) {
+                            setTheme(storedTheme === darkThemeClass);
+                        } else {
+                            setTheme(prefersDarkColorScheme);
+                            localStorage.setItem(storageKey, prefersDarkColorScheme ? darkThemeClass : lightThemeClass);
+                        }
+                    })();`}
                 </script>
             </Head>
             <body>
