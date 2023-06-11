ecdbbb297bb5106da35ecf5557e446dab24a86a4
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-06-11 18:56:43 +0100

    refactor: add meta tags and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import Script from 'next/script'
+import localFont from 'next/font/local'
+import type { Metadata } from 'next'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 
@@ -11,9 +13,63 @@ type IRootLayout = {
     children: React.ReactNode
 }
 
+const authorName = 'Julio Alves'
+const title = `${authorName} | Software Engineer`
+const description = 'Frontend developer experienced in building web-based applications for all kinds of devices.'
+const twitterHandle = '@juli0malves'
+
+export const metadata: Metadata = {
+    title,
+    description,
+    authors: [{ name: authorName }],
+    metadataBase: new URL('https://juliomalves.dev'),
+    alternates: {
+        canonical: 'https://juliomalves.dev'
+    },
+    openGraph: {
+        title,
+        description,
+        url: 'https://juliomalves.dev',
+        siteName: `${authorName} Website`,
+        // images: [],
+        locale: 'en_GB',
+        type: 'website'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title,
+        description,
+        creator: twitterHandle
+    },
+    icons: {
+        icon: [
+            { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+        ],
+        shortcut: '/favicon.ico',
+        apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }]
+    }
+}
+
+const latoFont = localFont({
+    src: [
+        {
+            path: './fonts/lato-regular.woff2',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path: './fonts/lato-bold.woff2',
+            weight: '700',
+            style: 'normal'
+        }
+    ],
+    display: 'optional'
+})
+
 const RootLayout = ({ children }: IRootLayout) => {
     return (
-        <html lang="en">
+        <html lang="en" className={latoFont.className}>
             <head>
                 <script id="theme" src="/scripts/theme.js"></script>
             </head>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react'
-import type { NextPage } from 'next'
-// import Head from 'next/head'
+import type { Metadata } from 'next'
 import PageWrapper from '@/components/page-wrapper'
 import Link from '@/components/link'
-// import { title } from '@/components/metadata'
 
-const Page404: NextPage = () => {
+const authorName = 'Julio Alves'
+const title = `${authorName} | Software Engineer`
+
+export const metadata: Metadata = {
+    title: `Page Not Found | ${title}`
+}
+
+const Page404 = () => {
     return (
-        <>
-            {/* <Head>
-                <title>{`Page Not Found - ${title}`}</title>
-            </Head> */}
-            <PageWrapper>
-                <section className="flex flex-col items-center place-content-center px-4 tablet:px-8" aria-labelledby="404">
-                    <h1 id="404">Page Not Found</h1>
-                    <p>
-                        <Link className="focus-outline" href="/">
-                            Go back home
-                        </Link>
-                    </p>
-                </section>
-            </PageWrapper>
-        </>
+        <PageWrapper>
+            <section className="flex flex-col items-center place-content-center px-4 tablet:px-8" aria-labelledby="404">
+                <h1 id="404">Page Not Found</h1>
+                <p>
+                    <Link className="focus-outline" href="/">
+                        Go back home
+                    </Link>
+                </p>
+            </section>
+        </PageWrapper>
     )
 }
 
