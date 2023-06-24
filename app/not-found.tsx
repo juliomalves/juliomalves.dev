e5238819f802f9e6a84c681d9e0fee9d18dee462
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2023-06-24 13:22:53 +0100

    fix: add document title to `not-found` page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,26 @@
 import * as React from 'react'
-import type { Metadata } from 'next'
 import PageWrapper from '@/components/page-wrapper'
 import Link from '@/components/link'
 
 const authorName = 'Julio Alves'
 const title = `${authorName} | Software Engineer`
 
-export const metadata: Metadata = {
-    title: `Page Not Found | ${title}`
-}
-
 const Page404 = () => {
     return (
-        <PageWrapper>
-            <section className="flex flex-col items-center place-content-center px-4 tablet:px-8" aria-labelledby="404">
-                <h1 id="404">Page Not Found</h1>
-                <p>
-                    <Link className="focus-outline" href="/">
-                        Go back home
-                    </Link>
-                </p>
-            </section>
-        </PageWrapper>
+        <>
+            {/* No support for metadata in `not-found.tsx` yet: https://github.com/vercel/next.js/pull/47328#issuecomment-1488891093 */}
+            <title>{`Page Not Found | ${title}`}</title>
+            <PageWrapper>
+                <section className="flex flex-col items-center place-content-center px-4 tablet:px-8" aria-labelledby="404">
+                    <h1 id="404">Page Not Found</h1>
+                    <p>
+                        <Link className="focus-outline" href="/">
+                            Go back home
+                        </Link>
+                    </p>
+                </section>
+            </PageWrapper>
+        </>
     )
 }
 
