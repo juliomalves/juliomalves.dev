24ea99320a46fe5d9237c9306b7ad1b2c902e699
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2024-08-25 17:25:58 +0100

    test: update `pages/layout` tests

---
+++ tests/specs/pages/layout.test.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { render, screen } from '@testing-library/react'
+import RootLayout from '@/app/layout'
+
+jest.mock('@/helpers/analytics')
+
+const renderComponent = ({ Component }: any) =>
+    render(
+        <RootLayout>
+            <Component />
+        </RootLayout>
+    )
+
+describe('GIVEN an <RootLayout />', () => {
+    describe('WHEN provided with a component and page props', () => {
+        it('THEN should render its children components', () => {
+            const Component = () => <main>Page component test</main>
+            renderComponent({ Component })
+            expect(screen.getByRole('banner')).toBeInTheDocument()
+            expect(screen.getByText('Page component test')).toBeInTheDocument()
+            expect(screen.getByRole('contentinfo')).toBeInTheDocument()
+        })
+    })
+})
