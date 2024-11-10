214987bdff65a0f5b28271dbd0ad1614071fbbcb
Julio Alves <25082183+juliomalves@users.noreply.github.com>
2024-11-10 13:12:23 +0000

    chore: convert Next config file to TypeScript

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
+import type { NextConfig } from 'next'
 import nextBundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = nextBundleAnalyzer({
     enabled: process.env.ANALYZE_BUNDLE === 'true'
 })
 
-/** @type {import('next').NextConfig} */
-const config = {
+const config: NextConfig = {
     output: 'export',
     images: {
         unoptimized: true
